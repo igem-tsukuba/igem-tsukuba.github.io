d4e3506efa53c4f901e995d80a370c54d920c078
user name <adress>
2024-10-06 17:43:59 +0900

    デザイン/スペル修正

--- src/legacy/App.tsx
+++ src/legacy/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '../componets/button/Button';
+import Button from '../components/button/Button';
 
 function App() {
   return (
