185b85f4c777f5f5657ba6cc2f854bdd46d77342
Hinami <s2310920@u.tsukuba.ac.jp>
2024-10-06 18:54:27 +0900

    wip

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../../public/components/footer/Instagram_Glyph_White.png'
+import '../../../public/components/footer/icon_mail.png'
+import '../../../public/components/footer/logo-white.png'
+
+//const Footer
