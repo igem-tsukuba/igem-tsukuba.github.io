b9283291a82a5355f8f85148fd696e54edd97e82
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-14 16:14:16 +0900

    コンポーネントの雛形を作成 wip

--- src/components/header_mini_menu/HeaderMiniMenu.tsx
+++ src/components/header_mini_menu/HeaderMiniMenu.tsx
@@ -1,1 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
+
+type HeaderMiniMenuProps = {
+    categoryColor: string;
+    bigTabName_ja: string;
+    bigTabName_en: string;
+    bigTabURL: string;
+    numberOfSmallTabs: number;
+    smallTabName?: string[];
+    smallTabURL?: string[];
+}
+
+const HeaderMiniMenu: React.FC<HeaderMiniMenuProps> = ({ categoryColor, bigTabName_ja, bigTabName_en, bigTabURL, numberOfSmallTabs, smallTabName, smallTabURL}) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleClick = () =>{
+        window.location.href = bigTabURL;
+    };
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+    return(
+        <button style={{width: "240px", height: "100px"}}>
+            <p> {bigTabName_ja} </p>
+            <p> {bigTabName_en} </p>
+        </button>
+    )
+};
+
+export default HeaderMiniMenu;
