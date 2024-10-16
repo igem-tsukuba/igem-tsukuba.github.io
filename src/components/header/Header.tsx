42b953f4e8f8a99cb9c6d9f78124a7108964cde5
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-16 18:44:02 +0900

    デザインwip

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../button/Button";
+import HeaderMiniMenu from "../header_mini_menu/HeaderMiniMenu";
 
 type HeaderProps = {
     pageTitle: string;
@@ -8,10 +8,22 @@ type HeaderProps = {
 const Header : React.FC<HeaderProps> = ({ pageTitle }) => {
     return(
         <div>
-            <div style={{ height: "80px", backgroundColor: "#691C70", }}>
-                <Button label="お問い合わせ" textColor="white" backgroundColor="#691C70" link="mailto:igemtsukuba24@gmail.com"/>
+            {/* 中央のタイトルの部分 */}
+            <div style = {{height: "100px"}}/>
+            <div style={{ display: "flex", justifyContent: "center",}}>
+                <img src={`${process.env.PUBLIC_URL}/components/header/iGEM_TSUKUBA_Logo.png`} alt="iGEM TSUKUBAのロゴ" style={{width: '70px', height: '70px', objectFit: 'cover', padding: "15px"}} />
+                <h1 style={{fontFamily: "Noto Sans JP", fontWeight: "600", fontSize: "40pt", margin: "10px"}}>{pageTitle}</h1>
             </div>
-            <p>{pageTitle}</p>
+            {/* 下の小メニューの部分 */}
+            <div style = {{display: "flex", justifyContent: "space-between"}}>
+                <HeaderMiniMenu categoryColor="#D20000" bigTabName_ja="私たちについて" bigTabName_en="About us" bigTabURL="" smallTab={[
+                        {
+                            smallTabLabels: ['チーム紹介', 'プロジェクト'],
+                            smallTabURLs: ['https://example.com/team', 'https://example.com/project'],
+                            content: undefined
+                        }
+                    ]} />
+            </div>
         </div>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
+import UpperHeader from '../components/header/UpperHeader';
 
 
 const Home: React.FC = () => {
     return (
         <div>
+            <UpperHeader />
             <Header pageTitle='ニュース'></Header>
             <p>WIP</p>
             <Footer />
