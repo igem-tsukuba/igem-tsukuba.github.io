f6c5a23871feca45fd1b50b29024781e6e27ffb1
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-18 22:39:26 +0900

    デザイン修正・完了

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -7,15 +7,29 @@ type ImageProps = {
     width?: number;
     height?: number;
     borderRadius?: number;
-    };
+    margin?: string;
+};
 
-const Image: React.FC<ImageProps> = ({ src, alt, caption, width = 280, height = 210, borderRadius = 15 }) => {
+const Image: React.FC<ImageProps> = ({ src, alt, caption, width = 280, height = 210, borderRadius = 15, margin ="30px 30px"}) => {
     return (
-        <div style={{width: width}}>
-            <figure>
-                <img src={`${process.env.PUBLIC_URL}/${src}`} alt={alt} width={width} height={height} style={{ borderRadius: borderRadius, objectFit: 'cover'}} />
-                <figcaption style={{fontFamily: "Noto Sans JP", width: width, textAlign: "center"}}>{caption}</figcaption>
-            </figure>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", }}>
+            <div style={{ width: width, textAlign: "center", margin: margin }}>
+                <figure style={{ margin: 0 }}>
+                    <img 
+                        src={`${process.env.PUBLIC_URL}/${src}`} 
+                        alt={alt} 
+                        width={width} 
+                        height={height} 
+                        style={{ 
+                            borderRadius: borderRadius, 
+                            objectFit: 'cover', 
+                            width: '100%', 
+                            height: '100%' 
+                        }} 
+                    />
+                    <figcaption style={{ fontFamily: "Noto Sans JP", width: width, textAlign: "center" }}>{caption}</figcaption>
+                </figure>
+            </div>
         </div>
     );
 }
