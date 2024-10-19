d4abb2f401a81a6901c85ce6b4a0181fadec6beb
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-19 16:40:21 +0900

    NewsPieceコンポーネントの修正(画像投稿可能に) テスト環境の作成

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import UpperHeader from '../components/header/UpperHeader';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import NewsPiece from '../components/news_piece/NewsPiece';
 
 const News: React.FC = () => {
     return (
-        <p>WIP</p>
+        <div>
+            <UpperHeader />
+            <Header pageTitle='ニュース' />
+            <NewsPiece title='タイトル' date='2021/09/01' control={1} content='本文' pictureSrc='logo512.png' pictureAlt='ニュース画像' topBar={false} />
+            <NewsPiece title='タイトル' date='2021/09/01' control={2} content='本文' pictureSrc='components/header/iGEM_TSUKUBA_Logo.png' pictureAlt='ニュース画像' topBar={true} />
+            <Footer />
+        </div>
     );
 }
 
