4ac0e0355bea91bab687a18d360abe4139aae425
user name <adress>
2024-10-22 16:02:33 +0900

    ボタン、パラグラフコンポーネントの導入

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,56 @@
 import React from 'react';
-import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
 import UpperHeader from '../components/header/UpperHeader';
+import Header from '../components/header/Header';
+import Subtitle from '../components/subtitle/Subtitle';
+import Paragraph from '../components/paragraph/Paragraph';
 import Newsfeed from '../components/news_piece/Newsfeed';
+import Button from '../components/button/Button';
+import Footer from '../components/footer/Footer';
 
 
 const Home: React.FC = () => {
     return (
         <div>
-            <UpperHeader />
-            <Header pageTitle='iGEM TSUKUBA　'></Header>
-            <Newsfeed />
-            <Footer />
+            <UpperHeader/>
+            <Header 
+                pageTitle='iGEM TSUKUBA　'/>
+            <Newsfeed/>
+
+            <Subtitle
+                text='好奇心旺盛な皆さんを募集しています！'/>
+            <Paragraph
+                text=''/>
+            <Button
+                label='iGEMでできること'
+                textColor='#691C70'
+                backgroundColor='#FFFFFF'
+                link=''
+                margin='30px'/>
+            
+            <Subtitle
+                text='iGEM TSUKUBAには皆様の温かいご支援が必要です'/>
+            <Paragraph
+                text=''/>
+            <Button
+                label='もっと見る'
+                textColor='#691C70'
+                backgroundColor='#FFFFFF'
+                link='/Sponsorship'
+                margin='30px'/>
+            <Button
+                label='クラウドファンディングはこちら'
+                textColor='#691C70'
+                backgroundColor='#FFFFFF'
+                link='https://camp-fire.jp/projects/787804/view'
+                margin='30px'/>
+            <Button
+                label='お問い合わせはこちら'
+                textColor='#691C70'
+                backgroundColor='#FFFFFF'
+                link=''
+                margin='30px'/>
+
+            <Footer/>
         </div>
     );
 }
