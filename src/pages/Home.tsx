e13112fb08d24f43b32d7e649c955e511bb450f2
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-24 10:02:43 +0900

    Homeへの実装がほぼ完了(あとは文章修正)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,22 @@ const Home: React.FC = () => {
             <UpperHeader />
             <Header pageTitle='iGEM TSUKUBA　'></Header>
             <Newsfeed />
-            <Banner src="pages/Home/education.webp" alt="Eduの写真" text="Education頑張ってます" buttonLabel="詳しくはこちら" buttonColor="#000000" link="/news" />
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <Banner 
+                    src="pages/Home/education.webp" 
+                    alt="Eduの写真" 
+                    text="Education頑張ってます、あーだこーだあーだこーだあーだこーだ" 
+                    buttonLabel="詳しくはこちら" 
+                    buttonColor="#F59A23" 
+                    link="/news" />
+                <Banner 
+                    src="pages/Sponsership/cloudfinding.webp" 
+                    alt="Eduの写真" 
+                    text="クラファンやってるよ！" 
+                    buttonLabel="詳しくはこちら" 
+                    buttonColor="#007AFF" 
+                    link="/sponsorship" />
+            </div>
             <Footer />
         </div>
     );
