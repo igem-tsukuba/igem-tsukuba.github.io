0b9a9a1914e3ae0a9dfb1da5b2f9ebca7f166927
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-24 11:19:58 +0900

    Paragraphタグの改行対応

--- src/components/paragraph/Paragraph.tsx
+++ src/components/paragraph/Paragraph.tsx
@@ -1,43 +1,51 @@
 import React from 'react';
-  
+
 type ParagraphProps = {
+  text: string | string[];
+  isBulletList?: boolean;
+};
 
-    text: string | string[];
-    isBulletList?: boolean;
+const Paragraph: React.FC<ParagraphProps> = ({ text, isBulletList = false }) => {
+  const renderText = (text: string | string[]) => {
+    if (Array.isArray(text)) {
+      return text.join(' ');
+    } else {
+      return text.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+          {line}
+          <br />
+        </React.Fragment>
+      ));
+    }
+  };
 
-
-};  
-
-const Paragraph: React.FC<ParagraphProps>  = ({
-    text, isBulletList = false
-}) => {
   return (
-      <p
-        style={{
-          fontFamily: 'Noto Sans JP',
-          fontSize: '14pt',
-          color: 'black',
-          backgroundColor: 'white',
-          paddingTop: '10pt',
-          paddingBottom: '10pt',
-          paddingLeft: '20pt',
-          paddingRight: '20pt',
-          marginTop: '10pt',
-          marginLeft: '10%',
-          marginRight: '10%',
-          textAlign: 'left',
-        }}
-      >
-        {isBulletList && Array.isArray(text) ? (
+    <p
+      style={{
+        fontFamily: 'Noto Sans JP',
+        fontSize: '14pt',
+        color: 'black',
+        backgroundColor: 'white',
+        paddingTop: '10pt',
+        paddingBottom: '10pt',
+        paddingLeft: '20pt',
+        paddingRight: '20pt',
+        marginTop: '10pt',
+        marginLeft: '10%',
+        marginRight: '10%',
+        textAlign: 'left',
+      }}
+    >
+      {isBulletList && Array.isArray(text) ? (
         <ul>
           {text.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
       ) : (
-        <>{Array.isArray(text) ? text.join(' ') : text}</>
+        renderText(text)
       )}
-      </p>
+    </p>
   );
 };
 
