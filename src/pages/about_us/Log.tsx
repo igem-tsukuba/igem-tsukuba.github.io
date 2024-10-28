1f358318ae799dec51777b454028240160befe56
user name <adress>
2024-10-29 00:46:41 +0900

    コンポーネントの導入

--- src/pages/about_us/Log.tsx
+++ src/pages/about_us/Log.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import UpperHeader from '../../components/header/UpperHeader';
+import Header from '../../components/header/Header';
+import Title from '../../components/title/Title';
+import Button from '../../components/button/Button';
+import Footer from '../../components/footer/Footer';
 
 const AboutIGEM: React.FC = () => {
     return (
-        <p>WIP</p>
+        <div>
+            <UpperHeader/>
+            <Header
+                pageTitle='活動記録'/>
+            <Title
+                text='これまでの活動実績'/>
+            
+            <Button
+                label='Newsはこちら'
+                textColor='#D20000'
+                backgroundColor='#FFFFFF'
+                link='/news'/>
+            <Footer/>
+        </div>
     );
 }
 
