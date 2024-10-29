30b7d578b1968cf14a368a891c1cfbb4d6b8fd98
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-29 20:52:02 +0900

    レスポンシブ対応/パラグラフ1

--- src/components/paragraph/Paragraph.tsx
+++ src/components/paragraph/Paragraph.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useScreenSizes } from "../WindowSizeUtils";
 
 type ParagraphProps = {
   text: string | string[];
@@ -6,6 +7,7 @@ type ParagraphProps = {
 };
 
 const Paragraph: React.FC<ParagraphProps> = ({ text, isBulletList = false }) => {
+  const { isMobileScreen, isSmallScreen } = useScreenSizes();
   const renderText = (text: string | string[]) => {
     if (Array.isArray(text)) {
       return text.join(' ');
@@ -23,16 +25,10 @@ const Paragraph: React.FC<ParagraphProps> = ({ text, isBulletList = false }) => 
     <p
       style={{
         fontFamily: 'Noto Sans JP',
-        fontSize: '14pt',
+        fontSize: isMobileScreen ? '10pt' : isSmallScreen ? '11pt' : '14pt',
         color: 'black',
         backgroundColor: 'white',
-        paddingTop: '10pt',
-        paddingBottom: '10pt',
-        paddingLeft: '20pt',
-        paddingRight: '20pt',
-        marginTop: '10pt',
-        marginLeft: '10%',
-        marginRight: '10%',
+        margin: '15pt 10%',
         textAlign: 'left',
       }}
     >
