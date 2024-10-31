c67b25817a59bf4cf0f38a7577e998f5d764b85f
Masahiro Suzuki <masahiro.cl419.421@gmail.com>
2024-10-31 19:30:15 +0900

    Recruitmentのレイアウト修正

--- src/pages/Recruitment.tsx
+++ src/pages/Recruitment.tsx
@@ -6,8 +6,10 @@ import Paragraph from '../components/paragraph/Paragraph';
 import Image from '../components/image/Image';
 import Button from '../components/button/Button';
 import Footer from '../components/footer/Footer';
+import { useScreenSizes } from '../components/WindowSizeUtils';
 
 const Recruitment: React.FC = () => {
+    const { isMobileScreen } = useScreenSizes();
     const bulletPoints = [
         '生物が好き',
         '大学で研究をはじめたい',
@@ -28,6 +30,23 @@ const Recruitment: React.FC = () => {
             text='私たちは新しい仲間を募集しています！'/>
         
         <div style={{display: "flex", justifyContent: "start" }}>
+        <Paragraph
+            text={bulletPoints} isBulletList={true}/>
+        <div style={{display: isMobileScreen ? "none" : ""}}>
+            <Image
+                src='pages/Recruitment/icons.webp'
+                alt='アイコン集合！'
+                caption=''
+                width={400}
+                height={300}
+                borderRadius={0}
+                margin='0px'/>
+            </div>
+        </div>
+
+        <Paragraph
+            text='iGEM TSUKUBAは、志高い新メンバーを募集しています！生物に自信がなくても大丈夫。あなたの力が必要です。一緒に金メダルを目指してみませんか？'/>
+        <div style={{display: isMobileScreen ? "" : "none" }}>
         <Image
             src='pages/Recruitment/icons.webp'
             alt='アイコン集合！'
@@ -36,29 +55,23 @@ const Recruitment: React.FC = () => {
             height={300}
             borderRadius={0}
             margin='0px'/>
-        <Paragraph
-            text={bulletPoints} isBulletList={true}/>
-        </div>
-
-        <Paragraph
-            text='iGEM TSUKUBAは、志高い新メンバーを募集しています！生物に自信がなくても大丈夫。あなたの力が必要です。一緒に金メダルを目指してみませんか？'/>
-
+        </div>    
         <Subtitle
            text='具体的な活動内容'/>
         <Paragraph
             text='合成生物学の世界大会であるiGEMへの出場を目指し、日々実験・研究やミーティングを重ねています。実験研究活動以外にも、メンバーの母校や地域での教育活動、企業との連携等、幅広く活動しています。研究や実験はもちろん、学外での活動など他の人とは一味違う大学生活を送れます。生物学に詳しくなくても、理系でなくても熱意があれば大丈夫！あなたの得意を活かして私たちと世界を目指しませんか？'/>
         
-        <div style={{display: "flex", justifyContent: "start" }}>
-        <Button
-            label='具体的な活動内容はこちら（iGEMとは）'
-            textColor='#00A400'
-            backgroundColor='#FFFFFF'
-            link='http://localhost:3000/about/about_iGEM'/>
-        <Button
-            label='メンバー紹介はこちら（メンバー）'
-            textColor='#00A400'
-            backgroundColor='#FFFFFF'
-            link='/about/members'/>
+        <div style={{display: isMobileScreen ? "block" : "flex", justifyContent: "start" }}>
+            <Button
+                label='具体的な活動内容はこちら（iGEMとは）'
+                textColor='#00A400'
+                backgroundColor='#FFFFFF'
+                link='/about/about_iGEM'/>
+            <Button
+                label='メンバー紹介はこちら（メンバー）'
+                textColor='#00A400'
+                backgroundColor='#FFFFFF'
+                link='/about/members'/>
         </div>
 
         <Image
